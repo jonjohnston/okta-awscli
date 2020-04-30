2e66a07f01830341b31ea39231ab2fc96afd8f54
Jonathan Johnston <jonathanjohnston@JJohnston-mbpr15-17554.local>
2020-04-30 09:37:20 -0400

    Adding files

---
+++ okta-cred-installer.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+BASEDIR=$(dirname "$0")
+user=$USER
+local=$(pwd)
+unameOut="$(uname -s)"
+case "${unameOut}" in
+	Linux*)     machine=Linux;;
+	Darwin*)    machine=Mac;;
+	CYGWIN*)    machine=Cygwin;;
+	MINGW*)     machine=MinGw;;
+	*)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ $machine != "Mac" ]; then
+	if (whoami != root); then 
+		echo "Need to run as sudo"
+	fi
+fi
+
+LOGFILE="${local}/installer.log"
+> $LOGFILE
+# Check prerequisites to make sure they are installed
+aws --version > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	rm -f ~/.aws/config ~/.aws/credentials >/dev/null 2>&1
+	aws --version > /dev/null 2>&1
+	if [ $? -ne 0 ]; then
+        	echo 'Warning: AWS CLI is not installed. Make sure to install that'
+        	exit 1
+	fi
+fi
+python --version > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo 'Warning: Python is required. Make sure to install that'
+        exit 1
+fi
+pip --version > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+        echo 'Warning: pip is required. Make sure to install that'
+        exit 1
+fi
+git --version > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+        echo 'Warning: GIT is required. Make sure to install that'
+        exit 1
+fi
+aliyuncheck=$(which aliyun)
+if [ -z "$aliyuncheck" ]; then
+	echo "Warning: Aliyun and jq are required if using Alibaba CLI"
+	read -n 1 -s -r -p "Press any key to continue"
+fi
+
+# Download from git and run the installer
+if [ ! -d ~/okta-awscli ]; then
+	git clone https://github.com/jonjohnston/okta-awscli.git ~/okta-awscli/ >/dev/null 2>&1
+	cd ~/okta-awscli/
+	python setup.py install >/dev/null 2>&1
+	rm -rf ~/okta-awscli
+fi
+
+cd $local
+# Copy files
+if [ ! -f ~/.okta-aws ]; then
+	cp $BASEDIR/.okta-aws ~/ >/dev/null 2>>$LOGFILE
+fi
+if [ ! -d ~/.aliyun ]; then
+	mkdir -p ~/.aliyun 2>>$LOGFILE
+fi
+chmod +x $BASEDIR/okta-cli >/dev/null 2>>$LOGFILE
+chmod +x $BASEDIR/aliyun-cli >/dev/null 2>>$LOGFILE
+cp $BASEDIR/okta-cli /usr/local/bin >/dev/null 2>&1
+cp $BASEDIR/aliyun-cli /usr/local/bin >/dev/null 2>>$LOGFILE
+cp $BASEDIR/aliyun.py /usr/local/bin >/dev/null 2>>$LOGFILE
+cp $BASEDIR/aliyun-config.json ~/.aliyun/ >/dev/null 2>>$LOGFILE
+
+# Check for errors
+errors=$(cat $LOGFILE)
+if [ "$errors" = "" ]; then
+	echo "Install has completed successfully. Run okta-cli"
+	rm -f $LOGFILE
+else
+	echo "Install completed with errors. Please check $LOGFILE"
+fi
